9b5cc6cc5cda8c81739a666061039fe1ce35677c
Liubov Matveeva <mtvliuba@gmail.com>
2025-05-08 22:04:48 +0500

    init

---
+++ src/test/java/ru/lumat/catalogueservice/CatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lumat.catalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
