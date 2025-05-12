40812e6f8cad0cf4f84bf37d0c91df8f9e80e75b
kharipova liubov <mtvliuba@gmail.com>
2025-05-12 21:45:24 +0500

    catalogue: - настроен security при обращении с другого сервиса

--- src/main/java/ru/lumat/catalogueservice/CatalogueServiceApplication.java
+++ src/main/java/ru/lumat/catalogueservice/CatalogueServiceApplication.java
@@ -2,6 +2,7 @@ package ru.lumat.catalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CatalogueServiceApplication {
